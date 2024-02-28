c91fc0b3da442a437bcda5a093e4f29bbe27b848
GauravSMarathe <147032653+GauravSMarathe@users.noreply.github.com>
2024-02-28 16:05:15 +0530

    Add files via upload

---
+++ FacultyInformation.java
@@ -0,0 +1,25 @@
+package fundamentals;
+
+
+	class Employee {
+	    void display() {
+	        System.out.println("Employee Information");
+	    }
+	}
+
+	class Faculty extends Employee {
+	    // Overriding the display method in the Faculty class
+	    @Override
+	    void display() {
+	        System.out.println("Faculty Information");
+	    }
+	}
+
+	public class FacultyInformation {
+	    public static void main(String[] args) {
+	        Faculty faculty = new Faculty();
+	        faculty.display();
+	    }
+	}
+
+
